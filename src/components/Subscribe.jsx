9ec736d17994e97540378013aab798729d30d540
JainishGamit <jainishgamit374@gmail.com>
2024-07-25 21:07:55 +0530

    Modify height and hover animation

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Subscribe = () => {
   return (
     <div className='w-[94vw] p-10 bg-[#dbdbdb]'>
-        <div className='my-20 text-white flex flex-col items-center justify-center py-5 bg-black'>
+        <div className='my-20 w-full h-[80vh] text-white flex flex-col items-center justify-center py-5 bg-black'>
             <h2 className='text-[3vw] uppercase tracking-tight font-semibold '>Upgrade your reading exprience</h2>
             <p className='text-[1.3vw] opacity-90'>Subscribe to get access to our exclusive Vogue reader app</p>
             <div
@@ -18,7 +18,7 @@ const Subscribe = () => {
                         top: 50,
                     })
                 }}
-            className='w-fit relative border mt-5 overflow-hidden hover:text-black '>
+            className='w-fit relative border mt-5 overflow-hidden hover:bg-white transition-all duration-[0.10] hover:text-black '>
                 <button className='px-4 py-2 relative z-10'>Subscribe</button>
                 <div className='up w-full h-full absolute left-0 bottom-50 bg-white'></div>
             </div>
